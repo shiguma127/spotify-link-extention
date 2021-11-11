285a009c77c6a347b3d998a768d7c904fa83c382
shiguma127 <shiguma127@gmail.com>
2021-11-12 00:27:54 +0900

    add:mui

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,1 +1,30 @@
-console.log("hello world");
+const observer = new MutationObserver(() => {
+  const lodingContainer: HTMLElement = document.querySelector(
+    ".js-app-loading"
+  ) as HTMLElement;
+  const isLoading = lodingContainer.style.display !== "none";
+  if (isLoading) return;
+  observer.disconnect();
+  const drawer = document.querySelector(".js-drawer");
+  const addImageButton = drawer.querySelector(".js-add-image-button");
+  const addSpotifyLinkButton = document.createElement("button");
+  addSpotifyLinkButton.className =
+    "btn btn-on-blue full-width txt-left margin-b--12 padding-v--6 padding-h--12";
+  addSpotifyLinkButton.dataset.originalTitle = "tabindex";
+  const icon = document.createElement("img");
+  icon.className = "Icon icon-camera txt-size--18";
+  //todo: add icon
+  //icon.src = chrome.runtime.getURL("icons/spotify.svg");
+  const span = document.createElement("span");
+  span.className = "label padding-ls";
+  span.innerText = "Add Spotify Link";
+  addSpotifyLinkButton.appendChild(icon);
+  addSpotifyLinkButton.appendChild(span);
+  addImageButton.after(addSpotifyLinkButton);
+});
+
+observer.observe(document.body, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
--- src/options.tsx
+++ src/options.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from "@mui/material/Button";
 
-const App = () => <div>App</div>;
-
-ReactDOM.render(<App />, document.getElementById("app"));
+const App = () => {
+  return <Button variant="contained">Hello World</Button>;
+};
+ReactDOM.render(<App />, document.getElementById("root"));
