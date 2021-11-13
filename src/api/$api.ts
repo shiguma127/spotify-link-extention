d434859ee7082339d280cf036e89a01af97cc30a
shiguma127 <shiguma127@gmail.com>
2021-11-14 00:51:09 +0900

    add:aspida

---
+++ src/api/$api.ts
@@ -0,0 +1,25 @@
+/* eslint-disable */
+// prettier-ignore
+import { AspidaClient } from 'aspida'
+// prettier-ignore
+import { Methods as Methods0 } from '.'
+
+// prettier-ignore
+const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
+  const prefix = (baseURL === undefined ? '' : baseURL).replace(/\/$/, '')
+
+  const GET = 'GET'
+
+  return {
+    get: (option?: { config?: T }) =>
+      fetch<Methods0['get']['resBody']>(prefix, '', GET, option).json(),
+    $get: (option?: { config?: T }) =>
+      fetch<Methods0['get']['resBody']>(prefix, '', GET, option).json().then(r => r.body),
+    $path: () => `${prefix}`
+  }
+}
+
+// prettier-ignore
+export type ApiInstance = ReturnType<typeof api>
+// prettier-ignore
+export default api
---
+++ src/api/index.ts
@@ -0,0 +1,7 @@
+import { PlayableItem } from "../../type/spotify/PlayableItem";
+
+export type Methods = {
+  get: {
+    resBody: PlayableItem;
+  };
+};
